c48bc8114c9c91afa063e29a3c7c1865dd25ce58
rodrigo <20400891k@utea.edu.pe>
2025-05-19 16:17:37 +0200

    formato 2

--- SalidaFormateada2.java
+++ SalidaFormateada2.java
@@ -1,1 +1,11 @@
 public class SalidaFormateada2 {
+    public static void main(String[] args) {
+        System.out.println("Articulo\tPrecio\tN° Cajas");
+        System.out.println("--------\t------\t--------");
+        System.out.printf("%-10s\t%8.2f\t%6d\n", "manzana",4.5,10);
+        System.out.printf("%-10s\t%8.2f\t%6d\n", "platano",7.3,10);
+        System.out.printf("%-10s\t%8.2f\t%6d\n", "uva",3.4,10);
+        System.out.printf("%-10s\t%8.2f\t%6d\n", "fresa",8.6,10);
+        System.out.printf("%-10s\t%8.2f\t%6d\n", "mandarina",6.2,10);
+    }
+}
