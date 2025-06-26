9611f2591a0e1a06e5e005bf91f340da5a89d117
rodrigo <20400891k@utea.edu.pe>
2025-06-26 19:42:18 +0200

    examen

---
+++ Ejercicios,java/ejercicio2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner Teclado = new Scanner(System.in);
+
+        int a;
+        System.out.println("Ingrese un numero del 1 al 100: ");
+        a= Teclado.nextInt();
+
+           if (a >= 1 && a <= 100) {
+            if (a % 3 == 0) {
+            if (a % 5 == 0) {
+                System.out.println("funf");
+            } else {
+                System.out.println("drei");
+                }
+            } else {
+            if (a % 5 == 0) {
+                System.out.println("funf");
+                } else {
+                System.out.println(a);
+                }
+            }
+        } else {
+            System.out.println("Error: número fuera de rango");
+        }
+    }
+}
